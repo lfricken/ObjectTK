9ccdb169d7cfdc8084eb22d610ae20dd228852ec
Chalant <strikerklm96@gmail.com>
2020-06-27 11:34:16 -0500

    Revert "Revert "breaks""
    This reverts commit 7c84739ab6071d8b6e85fb1b1a57c3b22a540420.

--- LeonTest/Program.cs
+++ LeonTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenTK;
+﻿using Examples.AdvancedExamples;
+using OpenTK;
 using OpenTK.Graphics.OpenGL;
 using System;
 
@@ -22,9 +23,14 @@ namespace LeonTest
             }
 
             // A simple vertex shader possible. Just passes through the position vector.
-            const string VertexShaderSource = @"
-            #version 330
+            const string VertexShaderSource =
+            @"#version 330
+// type
+
+
             in vec4 position;
+
+            uniform float CenterMass;
             void main(void)
             {
                 gl_Position = position;
@@ -32,8 +38,9 @@ namespace LeonTest
         ";
 
             // A simple fragment shader. Just a constant red color.
-            const string FragmentShaderSource = @"
-            #version 330
+            const string FragmentShaderSource =
+            @"#version 330
+// type
             out vec4 outputColor;
             void main(void)
             {
@@ -68,7 +75,7 @@ namespace LeonTest
 
             protected override void OnLoad(EventArgs e)
             {
-                // shader
+                // shaders
                 {
                     // Load the source of the vertex shader and compile it.
                     VertexShader = GL.CreateShader(ShaderType.VertexShader);
@@ -85,49 +92,91 @@ namespace LeonTest
                     GL.AttachShader(ShaderProgram, VertexShader);
                     GL.AttachShader(ShaderProgram, FragmentShader);
                     GL.LinkProgram(ShaderProgram);
+                    //var shader = new ShaderProgram();
+                    //shader.Attach(ShaderType.VertexShader, VertexShaderSource.Split("\r\n"));
+                    //shader.Attach(ShaderType.FragmentShader, FragmentShaderSource.Split("\r\n"));
+                    //shader.Link();
+
+                    //ShaderProgram = shader.Handle;
                 }
 
-                vao = GL.GenVertexArray();
-                vbo = GL.GenBuffer();
-                ebo = GL.GenBuffer();
 
-                // VAO
-                GL.BindVertexArray(vao);
+                // buffers
+                {
+                    vao = GL.GenVertexArray();
+                    vbo = GL.GenBuffer();
+                    ebo = GL.GenBuffer();
 
-                // VBO
-                GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-                GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(Points.Length * sizeof(float)), Points, BufferUsageHint.StaticDraw);
+                    // VAO
+                    GL.BindVertexArray(vao);
+                    GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+                    GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
 
-                // EBO
-                GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
-                GL.BufferData(BufferTarget.ElementArrayBuffer, new IntPtr(indices.Length * sizeof(int)), indices, BufferUsageHint.StaticDraw);
+                    GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(Points.Length * sizeof(float)), Points, BufferUsageHint.StaticDraw);
+                    GL.BufferData(BufferTarget.ElementArrayBuffer, new IntPtr(indices.Length * sizeof(int)), indices, BufferUsageHint.StaticDraw);
+
+                    var positionLocation = GL.GetAttribLocation(ShaderProgram, "position");
+                    GL.VertexAttribPointer(positionLocation, vertComponents, VertexAttribPointerType.Float, false, vboStride, 0);
+                    GL.EnableVertexAttribArray(positionLocation);
+
+                    // unbind for sanity so we don't accidentally write later
+                    GL.BindVertexArray(0);
+                    GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+                    GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+                }
 
 
-                var positionLocation = GL.GetAttribLocation(ShaderProgram, "position");
-                GL.VertexAttribPointer(positionLocation, vertComponents, VertexAttribPointerType.Float, false, vboStride, 0);
-                GL.EnableVertexAttribArray(positionLocation);
+                // random examples
+                {
+                    int count;
+                    // read shader variables
+                    GL.GetProgramInterface(ShaderProgram, ProgramInterface.ProgramInput, ProgramInterfaceParameter.ActiveResources, out count);
+                    for (int i = 0; i < count; ++i)
+                    {
+                        GL.GetActiveAttrib(ShaderProgram, 0, 256, out int len, out var size, out var type, out string name);
+                    }
 
-                // Set the clear color to blue
-                GL.ClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+                    GL.GetProgramInterface(ShaderProgram, ProgramInterface.Uniform, ProgramInterfaceParameter.ActiveResources, out count);
+                    for (int i = 0; i < count; ++i)
+                    {
+                        GL.GetActiveAttrib(ShaderProgram, 0, 256, out int len, out var size, out var type, out string name);
+                    }
+
+                    GL.GetProgramInterface(ShaderProgram, ProgramInterface.ShaderStorageBlock, ProgramInterfaceParameter.ActiveResources, out count);
+                    for (int i = 0; i < count; ++i)
+                    {
+                        GL.GetActiveAttrib(ShaderProgram, 0, 256, out int len, out var size, out var type, out string name);
+                    }
+                    GL.GetProgramInterface(ShaderProgram, ProgramInterface.UniformBlock, ProgramInterfaceParameter.ActiveResources, out count);
+                    for (int i = 0; i < count; ++i)
+                    {
+                        GL.GetActiveAttrib(ShaderProgram, 0, 256, out int len, out var size, out var type, out string name);
+                    }
+                }
+
+
+
+                // random final setup
+                {
+                    GL.PolygonMode(MaterialFace.Back, PolygonMode.Line); // make draw with lines if drawing backs
+                    GL.ClearColor(0.0f, 0.0f, 1.0f, 0.0f); // set clear color
+                }
 
                 base.OnLoad(e);
-
-
-
-                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-                GL.BindVertexArray(0);
             }
 
             protected override void OnUnload(EventArgs e)
             {
                 // Unbind all the resources by binding the targets to 0/null.
-                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
                 GL.BindVertexArray(0);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+                GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
                 GL.UseProgram(0);
 
                 // Delete all the resources.
-                GL.DeleteBuffer(vbo);
                 GL.DeleteVertexArray(vao);
+                GL.DeleteBuffer(vbo);
+                GL.DeleteBuffer(ebo);
                 GL.DeleteProgram(ShaderProgram);
                 GL.DeleteShader(FragmentShader);
                 GL.DeleteShader(VertexShader);
@@ -144,22 +193,13 @@ namespace LeonTest
 
             protected override void OnRenderFrame(FrameEventArgs e)
             {
-                ErrorCode error;
-                error = GL.GetError();
-
-                //Clear the color buffer.
                 GL.Clear(ClearBufferMask.ColorBufferBit);
-
-                // Bind the VBO
-                //GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-
 
                 GL.UseProgram(ShaderProgram);
                 GL.BindVertexArray(vao);
 
-                GL.PolygonMode(MaterialFace.Back, PolygonMode.Line);
+                // uses the EBO implicitly (actually it's in the name silly!)
                 GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
-
 
                 Context.SwapBuffers();
 
