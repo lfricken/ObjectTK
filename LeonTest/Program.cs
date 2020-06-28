1c0199ac35a89059a4dc328e78425524ba97327a
Chalant <strikerklm96@gmail.com>
2020-06-28 00:26:04 -0500

    try make batch

--- LeonTest/Program.cs
+++ LeonTest/Program.cs
@@ -2,7 +2,6 @@
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
 using System;
-using System.Diagnostics;
 
 namespace LeonTest
 {
@@ -17,11 +16,7 @@ namespace LeonTest
         }
         class Game : GameWindow
         {
-
-            public Game() : base(800, 800)
-            {
-
-            }
+            public Game() : base(800, 800) { }
 
             // A simple vertex shader possible. Just passes through the position vector.
             const string VertexShaderSource =
@@ -49,134 +44,31 @@ namespace LeonTest
             }
         ";
 
-
-            public interface IVertex
-            {
-                public unsafe void SetVertAttributes(int programHandle);
-            }
-
-
-            public struct Vertex : IVertex
-            {
-                public Vector2 position;
-                public float other;
-
-                public unsafe void SetVertAttributes(int programHandle)
-                {
-                    var vertexStride = sizeof(Vertex);
-                    int total = 0;
-                    {
-                        var numAttribs = 2;
-                        var layoutLocation = GL.GetAttribLocation(programHandle, nameof(position));
-                        GL.VertexAttribPointer(layoutLocation, numAttribs, VertexAttribPointerType.Float, false, vertexStride, total);
-                        GL.EnableVertexAttribArray(layoutLocation);
-                        total += sizeof(Vector2);
-                    }
-
-                    {
-                        var numAttribs = 1;
-                        var layoutLocation = GL.GetAttribLocation(programHandle, nameof(other));
-                        GL.VertexAttribPointer(layoutLocation, numAttribs, VertexAttribPointerType.Float, false, vertexStride, total);
-                        GL.EnableVertexAttribArray(layoutLocation);
-                        total += sizeof(float);
-                    }
-                }
-            }
-
-
+            SpriteBatch batch;
 
             // vArray of a triangle in normalized device coordinates.
-            readonly Vertex[] vArray = new Vertex[]
+            readonly IVertex[] vArray = new IVertex[]
             {
-                 new Vertex() { position = new Vector2(0, 0), other = 0 },  // top right
-                 new Vertex() { position = new Vector2(100, 0), other = 0 },  // top right
-                 new Vertex() { position = new Vector2(100, 100), other = 0 },  // top right
-                 new Vertex() { position = new Vector2(0, 100), other = 0.1f },  // top right
+                 new SpriteVertex() { position = new Vector2(0, 0), other = 0 },  // top right
+                 new SpriteVertex() { position = new Vector2(100, 0), other = 0 },  // top right
+                 new SpriteVertex() { position = new Vector2(100, 100), other = 0 },  // top right
+                 new SpriteVertex() { position = new Vector2(0, 100), other = 0.1f },  // top right
             };
-
-            // note that we start from 0!
-            readonly int[] indices = new int[]
-            {
-                0, 1, 2,   // first triangle
-                0, 2, 3,
-            };
-
-            ShaderProgram ShaderProgram;
-            int vao;
-            int vbo;
-            int ebo;
-            Matrix4 transform = QuadBatch.GenerateSpriteTransform(new Vector2(800, 800));
 
             protected override unsafe void OnLoad(EventArgs e)
             {
-                // shaders
                 {
-                    var shader = new ShaderProgram();
+                    var shader = new SpriteProgram(new Vector2(800, 800));
                     shader.Attach(ShaderType.VertexShader, VertexShaderSource.Split("\r\n"));
                     shader.Attach(ShaderType.FragmentShader, FragmentShaderSource.Split("\r\n"));
                     shader.Link();
 
-                    ShaderProgram = shader;
+                    batch = new SpriteBatch(shader, 1);
+
+                    batch.AddQuad(new SpriteData() { TopLeft = new Vector2(0, 0), BottomRight = new Vector2(100, 100), });
+
+                    batch.UpdateGpu();
                 }
-
-
-                // buffers
-                {
-                    vao = GL.GenVertexArray();
-                    vbo = GL.GenBuffer();
-                    ebo = GL.GenBuffer();
-
-                    // VAO
-                    GL.BindVertexArray(vao);
-                    GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-                    GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
-
-                    GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(vArray.Length * sizeof(Vertex)), vArray, BufferUsageHint.StaticDraw);
-                    GL.BufferData(BufferTarget.ElementArrayBuffer, new IntPtr(indices.Length * sizeof(int)), indices, BufferUsageHint.StaticDraw);
-
-                    new Vertex().SetVertAttributes(ShaderProgram.Handle);
-
-                    {
-                        var loc = GL.GetUniformLocation(ShaderProgram.Handle, "transform");
-                        Trace.Assert(loc != -1);
-                        GL.ProgramUniformMatrix4(ShaderProgram.Handle, loc, false, ref transform);
-                    }
-
-                    // unbind for sanity so we don't accidentally write later
-                    GL.BindVertexArray(0);
-                    GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-                    GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
-                }
-
-
-                // random examples
-                //{
-                //    int count;
-                //    // read shader variables
-                //    GL.GetProgramInterface(ShaderProgram, ProgramInterface.ProgramInput, ProgramInterfaceParameter.ActiveResources, out count);
-                //    for (int i = 0; i < count; ++i)
-                //    {
-                //        GL.GetActiveAttrib(ShaderProgram, 0, 256, out int len, out var size, out var type, out string name);
-                //    }
-
-                //    GL.GetProgramInterface(ShaderProgram, ProgramInterface.Uniform, ProgramInterfaceParameter.ActiveResources, out count);
-                //    for (int i = 0; i < count; ++i)
-                //    {
-                //        GL.GetActiveAttrib(ShaderProgram, 0, 256, out int len, out var size, out var type, out string name);
-                //    }
-
-                //    GL.GetProgramInterface(ShaderProgram, ProgramInterface.ShaderStorageBlock, ProgramInterfaceParameter.ActiveResources, out count);
-                //    for (int i = 0; i < count; ++i)
-                //    {
-                //        GL.GetActiveAttrib(ShaderProgram, 0, 256, out int len, out var size, out var type, out string name);
-                //    }
-                //    GL.GetProgramInterface(ShaderProgram, ProgramInterface.UniformBlock, ProgramInterfaceParameter.ActiveResources, out count);
-                //    for (int i = 0; i < count; ++i)
-                //    {
-                //        GL.GetActiveAttrib(ShaderProgram, 0, 256, out int len, out var size, out var type, out string name);
-                //    }
-                //}
-
 
 
                 // random final setup
@@ -190,17 +82,7 @@ namespace LeonTest
 
             protected override void OnUnload(EventArgs e)
             {
-                // Unbind all the resources by binding the targets to 0/null.
-                GL.BindVertexArray(0);
-                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-                GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
-                GL.UseProgram(0);
-
-                // Delete all the resources.
-                GL.DeleteVertexArray(vao);
-                GL.DeleteBuffer(vbo);
-                GL.DeleteBuffer(ebo);
-
+                batch.Dispose();
                 base.OnUnload(e);
             }
 
@@ -215,12 +97,7 @@ namespace LeonTest
             {
                 GL.Clear(ClearBufferMask.ColorBufferBit);
 
-                GL.BindVertexArray(vao);
-
-                ShaderProgram.Use();
-                // uses the EBO implicitly (actually it's in the name silly!)
-                GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
-
+                batch.Draw();
                 Context.SwapBuffers();
 
                 // Swap the front/back buffers so what we just rendered to the back buffer is displayed in the window.
--- LeonTest/QuadBatch.cs
+++ LeonTest/QuadBatch.cs
@@ -1,19 +1,127 @@
 ﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Examples.AdvancedExamples
 {
-    public class QuadBatch : IDisposable
+    public class QuadHandle
+    {
+        public int index;
+    }
+
+    public interface IVertex
+    {
+        public unsafe int GetSize();
+        public unsafe void SetVertAttributes(int programHandle);
+    }
+
+    public abstract class QuadBatch<TQuadData, TVertex> : IDisposable where TVertex : struct, IVertex
     {
         #region Lifecycle
-        public QuadBatch(ShaderProgram shader, int maxQuads)
-        {
+        List<QuadHandle> handlesToUpdate = new List<QuadHandle>();
+        List<QuadHandle> Handles = new List<QuadHandle>();
+        List<TQuadData> Data = new List<TQuadData>();
 
-        }
-        public static Matrix4 GenerateSpriteTransform(Vector2 vp)
+
+        public ShaderProgram Shader;
+        int vao;
+        int vbo; readonly TVertex[] vArray;
+        int ebo; readonly int[] iArray;
+
+
+        int numQuads = 0;
+
+        const int vPerQuad = 4;
+        const int iPerQuad = 6;
+        public QuadHandle AddQuad(TQuadData data)
         {
-            return Matrix4.CreateOrthographicOffCenter(0, vp.X, vp.Y, 0, 0, -1);
+            var newHandle = new QuadHandle() { index = Handles.Count };
+
+            Handles.Add(newHandle);
+            Data.Add(data);
+            UpdateQuad(newHandle);
+
+            return newHandle;
+        }
+
+        protected abstract TVertex GenVertex(TQuadData data, int offset);
+
+        public void UpdateQuad(QuadHandle _handle)
+        {
+            var handle = _handle.index;
+            var data = Data[handle];
+            Trace.Assert(handle < Handles.Count);
+
+            {
+                int[] verts = new int[4];
+                // vertexes
+                int vOffset = vPerQuad * handle;
+                for (int i = 0; i < 4; ++i)
+                {
+                    var vIndex = i + vOffset;
+                    verts[i] = vIndex;
+                    vArray[vIndex] = GenVertex(data, i);
+                }
+
+                // indexes to those vertexes
+                // see the index diagram at https://github.com/MonoGame/MonoGame/blob/develop/MonoGame.Framework/Graphics/SpriteBatcher.cs
+                // except we flipped vertex 2 and 3
+                int iOffset = iPerQuad * handle;
+                iArray[iOffset + 0] = verts[0];
+                iArray[iOffset + 1] = verts[1];
+                iArray[iOffset + 2] = verts[2];
+
+                iArray[iOffset + 3] = verts[0];
+                iArray[iOffset + 4] = verts[2];
+                iArray[iOffset + 5] = verts[3];
+            }
+
+            handlesToUpdate.Add(_handle);
+        }
+        public void Draw()
+        {
+            GL.BindVertexArray(vao);
+
+            Shader.Use();
+            // uses the EBO implicitly (actually it's in the name silly!)
+            GL.DrawElements(PrimitiveType.Triangles, numQuads * iPerQuad, DrawElementsType.UnsignedInt, 0);
+        }
+        public unsafe QuadBatch(ShaderProgram shader, int maxQuads)
+        {
+            var maxTriangles = maxQuads * 2;
+            vArray = new TVertex[maxQuads * vPerQuad];
+            iArray = new int[maxQuads * iPerQuad];
+
+            Shader = shader;
+            vao = GL.GenVertexArray();
+            vbo = GL.GenBuffer();
+            ebo = GL.GenBuffer();
+            new TVertex().SetVertAttributes(Shader.Handle);
+        }
+
+
+        public void UpdateGpu()
+        {
+            UpdateAllGpuBuffers();
+        }
+
+        private void UpdateAllGpuBuffers()
+        {
+            // VAO
+            GL.BindVertexArray(vao);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+
+            GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(vArray.Length * new TVertex().GetSize()), vArray, BufferUsageHint.StaticDraw);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, new IntPtr(iArray.Length * sizeof(int)), iArray, BufferUsageHint.StaticDraw);
+
+
+            // unbind for sanity so we don't accidentally write later
+            GL.BindVertexArray(0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
         }
         #endregion
 
@@ -25,7 +133,15 @@ namespace Examples.AdvancedExamples
         {
             if (!_isDisposed)
             {
-                //dispose
+                GL.BindVertexArray(0);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+                GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+                GL.UseProgram(0);
+
+                Shader.Dispose();
+                GL.DeleteVertexArray(vao);
+                GL.DeleteBuffer(vbo);
+                GL.DeleteBuffer(ebo);
             }
             _isDisposed = true;
         }
