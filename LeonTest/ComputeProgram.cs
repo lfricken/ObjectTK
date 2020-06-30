5ceeb762ba016445d829874b3d020c11a8b17abb
Chalant <strikerklm96@gmail.com>
2020-06-29 22:36:28 -0500

    create compute

---
+++ LeonTest/ComputeProgram.cs
@@ -0,0 +1,65 @@
+﻿using LeonTest;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Diagnostics;
+
+namespace Examples.AdvancedExamples
+{
+
+    public struct Cell
+    {
+        public float CleanAir;
+        public float DirtyAir;
+    }
+
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ComputeProgram : ShaderProgram
+    {
+        int ssbo0;
+        int ssbo1;
+
+        int sideLength;
+        int groups;
+
+        public unsafe void Make(int _workPerGroup, int _groups)
+        {
+            groups = _groups;
+            sideLength = _workPerGroup * _groups;
+            const string codePath = "../../../ComputeProgram.glsl";
+            Attach(ShaderType.ComputeShader, codePath);
+            Link();
+
+            var bufferSizeInBytes = sideLength * sizeof(Cell);
+            ssbo0 = GL.GenBuffer();
+            ssbo1 = GL.GenBuffer();
+
+            GL.BindBuffer(BufferTarget.ShaderStorageBuffer, ssbo0);
+            GL.BufferData(BufferTarget.ShaderStorageBuffer, new IntPtr(bufferSizeInBytes), new IntPtr(0), BufferUsageHint.StaticDraw);
+
+
+            var cellsa = GL.MapBufferRange(BufferTarget.ShaderStorageBuffer, new IntPtr(0), bufferSizeInBytes, BufferAccessMask.MapWriteBit | BufferAccessMask.MapInvalidateBufferBit);
+            var cells = (Cell*)cellsa.ToPointer();
+            for (int i = 0; i < sideLength; ++i)
+            {
+                cells[i].CleanAir = 1f;
+                cells[i].DirtyAir = 0.5f;
+            }
+            GL.UnmapBuffer(BufferTarget.ShaderStorageBuffer);
+        }
+
+
+        public override void Use()
+        {
+            GL.BindBufferBase(BufferRangeTarget.ShaderStorageBuffer, 4, ssbo0);
+
+            GL.UseProgram(Handle);
+            GL.DispatchCompute(groups, 1, 1);
+            GL.MemoryBarrier(MemoryBarrierFlags.ShaderStorageBarrierBit);
+        }
+    }
+}
---
+++ LeonTest/ComputeProgram.glsl
@@ -0,0 +1,36 @@
+﻿#version 440
+// type
+
+
+
+#if TypeIs_VertexShader
+uniform mat4 writeTransform;
+uniform vec2 readTransform;
+
+in vec4 position;
+in vec2 texCoord;
+
+out vec2 fragTexCoord;
+
+void main(void)
+{
+    fragTexCoord = readTransform * texCoord;
+    gl_Position = writeTransform * position;
+}
+#endif
+
+
+
+#if TypeIs_FragmentShader
+uniform sampler2D texture0;
+uniform sampler2D texture1;
+
+in vec2 fragTexCoord;
+
+out vec4 outputColor;
+
+void main(void)
+{
+    outputColor = mix(texture(texture1, fragTexCoord), texture(texture0, fragTexCoord), 0.8);
+}
+#endif
